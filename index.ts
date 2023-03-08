350c998ef381d002e14c2db621302e1737998bf2
JorgeVergaraSou <110506751+JorgeVergaraSou@users.noreply.github.com>
2023-03-08 00:09:16 -0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -2,13 +2,31 @@ import { Auto } from "./autos";
 import { RegistroAutomotor } from "./registroAuto";
 import * as fs from "fs";
 
-const informacion = fs.readFileSync("./bodega.json", "utf8");
+/* se lee archivo JSON y se asigna a una variable*/
+const informacion = fs.readFileSync("./bodega.json", "utf8"); 
+
+//se parsea la informacion del archivo
 const cansao = JSON.parse(informacion);
-//console.log(Datos);
-var vendedor = new RegistroAutomotor;
-var berme = new Auto ("toyota", "milano", "gasoil");
-   // console.log(berme);
+
+// se crea una instancia de la clase
+const vendedor = new RegistroAutomotor;
+
+/* en esta linea se crea un nuevo dato para insertar */
+const berme = new Auto ("CHEVY", "500", "GASOIL"); 
+
+/* en esta variable se ingresa la marca de auto a usar en las distintas funciones */
+const datoConsultas = "CHEVY"; 
+
+//const nuevoNombre = "nisato";
 
 
+/* PARA USAR ALGUNA FUNCION, DESCOMENTAR LA LINEA DESEADA Y COMENTAR EL RESTO */
 
-vendedor.insertar(berme, cansao )
+//vendedor.insertar(berme, cansao );
+
+//vendedor.consultar(datoConsultas, cansao);
+
+//EN CONSTRUCCION
+//vendedor.modificar(datoConsultas, cansao, nuevoNombre);
+
+//vendedor.eliminar(datoConsultas, cansao);
--- registroAuto.ts
+++ registroAuto.ts
@@ -1,3 +1,4 @@
+import { log } from "console";
 import { Auto } from "./autos";
 import * as fs from "fs";
 
@@ -7,9 +8,10 @@ export class RegistroAutomotor{
         if(array.push(auto)){
             console.log('Se ha añadido ', auto.marca, ' a la base de datos', array);
             fs.writeFileSync("./bodega.json", JSON.stringify(array)); 
+            fs.appendFileSync("./bitacora.txt", `FUNCION INSERTAR: Se modifico el Stock se agrego ${ auto.marca} \n`);
         } else{
-            console.log('El Auto', auto.marca, ' No se ha podido añadir a la concesionaria');
-            
+            console.log('El Auto', auto.marca, ' No se ha podido añadir a la concesionaria');   
+            fs.appendFileSync("./bitacora.txt", `FUNCION INSERTAR: no se pudo agregar ${ auto.marca} \n`);       
         }
     }
 
@@ -17,36 +19,44 @@ export class RegistroAutomotor{
         const autoEncontrado = array.find(auto => auto.marca === marca )
         if(autoEncontrado){
             console.log(marca, ' Existe en la concesionaria', autoEncontrado)
+            fs.appendFileSync("./bitacora.txt", `FUNCION CONSULTAR: Existe el vehiculo marca: ${ marca} \n`);
             return autoEncontrado
         } else{
             console.log(marca, ' No existe en la concesionaria');
-            
+            fs.appendFileSync("./bitacora.txt", `FUNCION CONSULTAR: El vehiculo marca:  ${ marca} no existe en la BD \n`);
         }
     }
-
+    /*
     modificar(marca: string, array: Auto[], dato: string){
-        const autoModificar = this.consultar(marca, array)
-        if(autoModificar){
-            autoModificar.marca = dato;
-            console.log('El Auto', marca, ' Ha sido modificado y ahora se llama ', dato);
-        }
-        else {
-            console.log('El Auto no se ha podido modificar');
-            
-        }
-        
-    }
 
+        const autoEncontrado = array.findIndex(auto => auto.marca == marca);
+        if(autoEncontrado >= 0){  
+
+
+            array.splice(autoEncontrado, 1)
+            console.log('Auto eliminado', marca);
+            fs.writeFileSync("./bodega.json", JSON.stringify(array));
+            fs.appendFileSync("./bitacora.txt", `FUNCION ELIMINAR: Se elimino el vehiculo ${marca}  \n`);
+            return array;
+        } else {
+
+         console.log('Auto ', marca, 'no ha sido eliminado');
+         fs.appendFileSync("./bitacora.txt", `FUNCION ELIMINAR: No se quito el vehiculo ${marca}  \n`);
+        }
+    }
+*/
     eliminar(marca: string, array: Auto[]): any {
         const autoEncontrado = array.findIndex(auto => auto.marca == marca);
         if(autoEncontrado >= 0){  
             array.splice(autoEncontrado, 1)
             console.log('Auto eliminado', marca);
-            console.log(array);
+            fs.writeFileSync("./bodega.json", JSON.stringify(array));
+            fs.appendFileSync("./bitacora.txt", `FUNCION ELIMINAR: Se elimino el vehiculo ${marca}  \n`);
             return array;
         } else {
 
          console.log('Auto ', marca, 'no ha sido eliminado');
+         fs.appendFileSync("./bitacora.txt", `FUNCION ELIMINAR: No se quito el vehiculo ${marca}  \n`);
         }
     }
 }
